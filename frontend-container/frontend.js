ff2be462d3783c5b9394e01e846927d9d07ce1d9
faoiltiarnadev <faoiltiarna.dev@gmail.com>
2017-05-25 20:19:43 +0100

    Added jade template engine and home view

--- frontend-container/frontend.js
+++ frontend-container/frontend.js
@@ -1,12 +1,35 @@
-var grpc = require('grpc');
+//var grpc = require('grpc');
 var express = require('express');
-
 var app = express();
 var router = express.Router();
+var compression = require('compression');
 
-var proto = grpc.load('interface.proto');
+//var proto = grpc.load('interface.proto');
+//var client = new proto.anagram.AnagramService('backend:50051');
 
-var client = new proto.anagram.AnagramService('backend:50051');
+router.get('/', function(request, response) {
+
+    response.render('home');
+
+    //getAnagram(response, request.query);
+ });
+
+// Set up the app to use a template engine
+// See https://expressjs.com/en/guide/using-template-engines.html for more information
+// Tells express where the template files are located
+app.set('views', __dirname + '/views');
+// Tells express that the template engine used will be jade
+app.set('view engine', 'jade');
+
+// Add content compression middleware
+app.use(compression());
+
+// Add static middleware
+var oneDay = 86400000;
+app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
+
+app.use(router);
+app.listen(3000);
 
 function getAnagram(response, query) {
 
@@ -24,22 +47,13 @@ function getAnagram(response, query) {
         word: word
     }
 
-    client.getAnagram(request, function(error, anagram) {
-        if (error) {
-            response.end(JSON.stringify(error));
-        } else {
-            response.end(anagram.question + " is an anagram of " + anagram.answer + "\n");
-        }
-    });
+    response.end("Hello! :)");
+
+    /* client.getAnagram(request, function(error, anagram) {
+     if (error) {
+     response.end(JSON.stringify(error));
+     } else {
+     response.end(anagram.question + " is an anagram of " + anagram.answer + "\n");
+     }
+     });*/
 }
-
-router.get('/', function(request, response) {
-    response.writeHead(200, {
-        "Content-Type": "text/plain"
-    });
-
-    getAnagram(response, request.query);
- });
-
-app.use(router);
-app.listen(3000);
